ccf63a0c24c758bfbb68c02448df59458414ce94
Rosenstein <4959837+Rosenstein@users.noreply.github.com>
2025-01-03 00:56:36 +0100

    Script update
    - cURL will check server signature using system's native CA store
    - New files will not be downloaded if unchanged on server

--- dlupdt.bat
+++ dlupdt.bat
@@ -1,18 +1,20 @@
 @echo off
+@SETLOCAL
+pushd "%~dp0"
 color 0A
 title Kaspersky Rescue Disk 18 Updater
-echo:
-echo:
-echo  Kaspersky Rescue Disk 18 Updater by Rosenstein                 
-echo     (based on the Bharat Balegere's updater from AgniPulse.Com) 
-echo:
-echo:
-echo  https://github.com/Rosenstein/Kaspersky-Rescue-Disk18-Updater  
-echo:
-echo:
-echo:
-echo:
-echo:
+echo ^ ________________________________________________________________
+echo ^|                                                                ^|
+echo ^| Kaspersky Rescue Disk 18 Updater by Rosenstein                 ^|
+echo ^|    (based on the Bharat Balegere's updater from AgniPulse.Com) ^|
+echo ^|                                                                ^|
+echo ^|                                                                ^|
+echo ^| https://github.com/Rosenstein/Kaspersky-Rescue-Disk18-Updater  ^|
+echo ^|                                                                ^|
+echo ^|                                                                ^|
+echo ^|                                                                ^|
+echo ^|                                                                ^|
+echo ^|________________________________________________________________^|
 :START
 if not exist .\Tools\mkisofs.exe goto mkiso
 if not exist .\Tools\7z.exe goto x
@@ -22,8 +24,7 @@ if exist .\042-freshbases.srm del .\042-freshbases.srm
 if exist .\hashes.txt del .\hashes.txt
 if exist .\005-bases.srm del .\005-bases.srm
 if exist .\005-bases.srm.sha512 del .\005-bases.srm.sha512
-if exist .\krd.xml del .\krd.xml
-if exist .\krd_new.iso del .\krd_new.iso
+
 
 echo Extracting contents of Kaspersky Rescue Disk
 Title Extracting Kaspersky Rescue Disk
@@ -35,9 +36,9 @@ if errorlevel 2 goto:fatal_error
 if errorlevel 1 goto:ok_warnings
 echo Kaspersky Files Extracted to %~dp0kavrescue
 :: Check krd version
-.\Tools\curl -# -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/bases/krd.xml" > nul 2>&1
-FOR /F delims^=^"^ tokens^=2 %%G IN ('findstr /R /C:"\<version\>" "krd.xml"')  DO set xmlver=%%G
-FOR /F delims^=^"^ tokens^=2 %%G IN ('findstr /R /C:"\<system_patch\>" "krd.xml"')  DO set xmlpatch=%%G
+.\Tools\curl -# --etag-compare .\Tools\xml_tag.txt --etag-save .\Tools\xml_tag.txt -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/bases/krd.xml" --ca-native > nul 2>&1
+for /F delims^=^"^ tokens^=2 %%G IN ('findstr /R /C:"\<version\>" "krd.xml"') do set xmlver=%%G
+for /F delims^=^"^ tokens^=2 %%G IN ('findstr /R /C:"\<system_patch\>" "krd.xml"') do set xmlpatch=%%G
 for /f "tokens=4 delims=() " %%F IN ('findstr /L "Kaspersky" ".\kavrescue\krd_version.txt"') do set isover=%%F
 for /f "tokens=5 delims=() " %%F IN ('findstr /L "Kaspersky" ".\kavrescue\krd_version.txt"') do set isopatch=%%F
 goto :check
@@ -48,23 +49,24 @@ echo:
 echo:
 Title Downloading fresh bases
 echo Downloading fresh bases for Kaspersky Rescue Disk
+echo Notice: New files will not be downloaded if they are unchanged on the server
 echo:
-.\Tools\curl -# -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/bases/042-freshbases.srm"
-.\Tools\curl -# -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/bases/hashes.txt"
+.\Tools\curl -# --etag-compare .\Tools\bases_tag.txt --etag-save .\Tools\bases_tag.txt -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/bases/042-freshbases.srm" --ca-native
+.\Tools\curl -# --etag-compare .\Tools\hashes_tag.txt --etag-save .\Tools\hashes_tag.txt -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/bases/hashes.txt" --ca-native
 
 echo:
 echo Renaming files!
-ren "042-freshbases.srm" "005-bases.srm" > nul 2>&1
-for /f "tokens=1" %%i in ('FINDSTR /L "042-freshbases.srm" "hashes.txt"') do echo %%i *005-bases.srm> 005-bases.srm.sha512
+ren "042-freshbases.srm" "005-bases.srm" 2> nul
+for /f "tokens=1" %%i in ('FINDSTR /L "042-freshbases.srm" "hashes.txt"') do echo %%i *005-bases.srm> 005-bases.srm.sha512 2> nul
 echo:
 echo:
 
 title Copying Updated Virus Definition Files to your Rescue Disk
-echo Copying Updated Virus Definition Base to your Rescue Disk
+echo Copying Updated Virus Definition Base to your Rescue Disk ( if available )
 copy /y .\005-bases.srm .\kavrescue\data\005-bases.srm > nul
 copy /y .\005-bases.srm.sha512 .\kavrescue\data\005-bases.srm.sha512 > nul
 echo:
-FOR /F delims^=^"^ tokens^=2 %%G IN ('FINDSTR /L "databases_timestamp" "krd.xml"')  DO ECHO %%G > .\kavrescue\krd_bases_timestamp.txt & echo: & echo Base timestamp is: %%G
+FOR /F delims^=^"^ tokens^=2 %%G IN ('FINDSTR /L "databases_timestamp" "krd.xml"')  DO ECHO %%G > .\kavrescue\krd_bases_timestamp.txt & echo: & echo Bases timestamp is: %%G
 echo Updating "krd_bases_timestamp.txt"
 echo:
 echo:
@@ -72,6 +74,7 @@ echo Successfully Copied Updated Definition Files to your Rescue Disk
 echo:
 echo:
 echo Next Step: Rebuilding the Rescue Disk ISO Image
+echo Notice: ISO rebuilding will be skipped if database version is unchanged
 echo:
 echo:
 title Creating the Rescue Disk ISO Image
@@ -81,16 +84,15 @@ SET CDBOOT=
 if exist .\kavrescue\boot\grub\i386-pc\eltorito.img set CDBOOT=boot/grub/i386-pc/eltorito.img 
 if exist .\kavrescue\boot\grub\grub_eltorito set CDBOOT=boot/grub/grub_eltorito 
 if "%CDBOOT%"=="" goto bs
-.\Tools\mkisofs -R -J -joliet-long -o krd_new.iso -b %CDBOOT% -c boot\boot.cat -no-emul-boot -boot-info-table -V "Kaspersky Rescue Disk" -boot-load-size 4 kavrescue
+.\Tools\mkisofs -R -J -joliet-long -o krd.iso -b %CDBOOT% -c boot\boot.cat -no-emul-boot -boot-info-table -V "Kaspersky Rescue Disk" -boot-load-size 4 kavrescue
 if errorlevel 1 goto :ERR
 echo:
 echo:
-echo NO ERRORS - New "krd_new.iso" IS CREATED!
+echo NO ERRORS - New "krd.iso" IS CREATED!
 rmdir /S /Q .\kavrescue
-del .\005-bases.srm
-del .\005-bases.srm.sha512
-del .\hashes.txt
-del .\krd.xml
+if exist .\005-bases.srm del .\005-bases.srm
+if exist .\005-bases.srm.sha512 del .\005-bases.srm.sha512
+if exist .\hashes.txt del .\hashes.txt
 echo:
 goto :end
 :ERR
@@ -116,12 +118,10 @@ goto :end
 echo:
 echo:
 echo Downloading KRD.iso from Kaspersky Labs server
-.\Tools\curl -# -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/krd.iso"
+.\Tools\curl -# -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/krd.iso" --ca-native
 echo:
-Echo krd.iso has been downloaded. Now update.
+Echo krd.iso has been downloaded. Now updating databases.
 if exist .\kavrescue rmdir /S /Q .\kavrescue
-if exist .\krd_new.iso del .\krd_new.iso
-if exist .\krd.xml del .\krd.xml
 echo:
 goto :start
 :mkiso
@@ -147,7 +147,7 @@ goto :end
 IF %xmlver%%xmlpatch% GTR %isover%%isopatch% (
 echo:
 echo It seems that there is a new version of Kaspersky Rescue Disk available!
-echo Version on the disk: %isover%%isopatch% Þ Version on the server: %xmlver%%xmlpatch%
+echo Version on the disk: %isover%%isopatch% ][ Version on the server: %xmlver%%xmlpatch%
 goto :input
 ) ELSE ( 
 goto :continue
