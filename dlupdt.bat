b1dd0a2517d6202696488d8ee711c94370fb9b25
Rosenstein <4959837+Rosenstein@users.noreply.github.com>
2025-01-04 00:16:32 +0100

    More checks added
    - ISO rebuilding will be skipped if database version is unchanged

--- dlupdt.bat
+++ dlupdt.bat
@@ -15,7 +15,9 @@ echo ^|                                                                ^|
 echo ^|                                                                ^|
 echo ^|                                                                ^|
 echo ^|________________________________________________________________^|
-:START
+echo:
+
+:start
 if not exist .\Tools\mkisofs.exe goto mkiso
 if not exist .\Tools\7z.exe goto x
 if not exist .\krd.iso goto y
@@ -24,8 +26,6 @@ if exist .\042-freshbases.srm del .\042-freshbases.srm
 if exist .\hashes.txt del .\hashes.txt
 if exist .\005-bases.srm del .\005-bases.srm
 if exist .\005-bases.srm.sha512 del .\005-bases.srm.sha512
-
-
 echo Extracting contents of Kaspersky Rescue Disk
 Title Extracting Kaspersky Rescue Disk
 .\Tools\7z x -o"kavrescue" -bsp2 -y -x"![BOOT]\*.img" "krd.iso" > nul
@@ -36,45 +36,48 @@ if errorlevel 2 goto:fatal_error
 if errorlevel 1 goto:ok_warnings
 echo Kaspersky Files Extracted to %~dp0kavrescue
 :: Check krd version
-.\Tools\curl -# --etag-compare .\Tools\xml_tag.txt --etag-save .\Tools\xml_tag.txt -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/bases/krd.xml" --ca-native > nul 2>&1
+.\Tools\curl -# -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/bases/krd.xml" --ca-native > nul 2>&1
 for /F delims^=^"^ tokens^=2 %%G IN ('findstr /R /C:"\<version\>" "krd.xml"') do set xmlver=%%G
 for /F delims^=^"^ tokens^=2 %%G IN ('findstr /R /C:"\<system_patch\>" "krd.xml"') do set xmlpatch=%%G
 for /f "tokens=4 delims=() " %%F IN ('findstr /L "Kaspersky" ".\kavrescue\krd_version.txt"') do set isover=%%F
 for /f "tokens=5 delims=() " %%F IN ('findstr /L "Kaspersky" ".\kavrescue\krd_version.txt"') do set isopatch=%%F
+for /f %%A in (.\kavrescue\krd_bases_timestamp.txt) do set isostamp=%%A
+for /F delims^=^"^ tokens^=2 %%H IN ('FINDSTR /L "databases_timestamp" "krd.xml"') do set xmlstamp=%%H
 goto :check
-:CONTINUE
+
+:continue
 echo:
 echo OK! Updating virus definitions for the KRD on the disk...
+echo Notice: New files will not be downloaded if they are unchanged on the server
+echo Notice: ISO rebuilding will be skipped if database version is unchanged
 echo:
 echo:
+goto :check2
+:cont1
 Title Downloading fresh bases
 echo Downloading fresh bases for Kaspersky Rescue Disk
-echo Notice: New files will not be downloaded if they are unchanged on the server
 echo:
 .\Tools\curl -# --etag-compare .\Tools\bases_tag.txt --etag-save .\Tools\bases_tag.txt -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/bases/042-freshbases.srm" --ca-native
 .\Tools\curl -# --etag-compare .\Tools\hashes_tag.txt --etag-save .\Tools\hashes_tag.txt -O "https://rescuedisk.s.kaspersky-labs.com/updatable/2018/bases/hashes.txt" --ca-native
-
 echo:
 echo Renaming files!
-ren "042-freshbases.srm" "005-bases.srm" 2> nul
-for /f "tokens=1" %%i in ('FINDSTR /L "042-freshbases.srm" "hashes.txt"') do echo %%i *005-bases.srm> 005-bases.srm.sha512 2> nul
+if exist 042-freshbases.srm ren "042-freshbases.srm" "005-bases.srm"
+if exist hashes.txt for /f "tokens=1" %%I in ('FINDSTR /L "042-freshbases.srm" "hashes.txt"') do echo %%I *005-bases.srm> 005-bases.srm.sha512
 echo:
 echo:
-
 title Copying Updated Virus Definition Files to your Rescue Disk
 echo Copying Updated Virus Definition Base to your Rescue Disk ( if available )
 copy /y .\005-bases.srm .\kavrescue\data\005-bases.srm > nul
 copy /y .\005-bases.srm.sha512 .\kavrescue\data\005-bases.srm.sha512 > nul
 echo:
-FOR /F delims^=^"^ tokens^=2 %%G IN ('FINDSTR /L "databases_timestamp" "krd.xml"')  DO ECHO %%G > .\kavrescue\krd_bases_timestamp.txt & echo: & echo Bases timestamp is: %%G
-echo Updating "krd_bases_timestamp.txt"
+echo %xmlstamp% > .\kavrescue\krd_bases_timestamp.txt & echo: & echo New Bases timestamp is: %xmlstamp%
+echo Updated ".\kavrescue\krd_bases_timestamp.txt"
 echo:
 echo:
 echo Successfully Copied Updated Definition Files to your Rescue Disk
 echo:
 echo:
 echo Next Step: Rebuilding the Rescue Disk ISO Image
-echo Notice: ISO rebuilding will be skipped if database version is unchanged
 echo:
 echo:
 title Creating the Rescue Disk ISO Image
@@ -85,24 +88,29 @@ if exist .\kavrescue\boot\grub\i386-pc\eltorito.img set CDBOOT=boot/grub/i386-pc
 if exist .\kavrescue\boot\grub\grub_eltorito set CDBOOT=boot/grub/grub_eltorito 
 if "%CDBOOT%"=="" goto bs
 .\Tools\mkisofs -R -J -joliet-long -o krd.iso -b %CDBOOT% -c boot\boot.cat -no-emul-boot -boot-info-table -V "Kaspersky Rescue Disk" -boot-load-size 4 kavrescue
+title Kaspersky Rescue Disk 18 Updater
 if errorlevel 1 goto :ERR
 echo:
 echo:
 echo NO ERRORS - New "krd.iso" IS CREATED!
+:cont2
 rmdir /S /Q .\kavrescue
 if exist .\005-bases.srm del .\005-bases.srm
 if exist .\005-bases.srm.sha512 del .\005-bases.srm.sha512
 if exist .\hashes.txt del .\hashes.txt
 echo:
 goto :end
-:ERR
+
+:err
 echo ERROR! Some problem occurred!
 pause
 goto :end
+
 :x
 echo Missing file  %~dp0Tools\7z.exe.
 echo Please re-extract "Tools" folder from downloaded zip file
 goto :end
+
 :y
 echo !! Kaspersky Rescue Disk Is Not Present !!.
 echo:
@@ -111,9 +119,11 @@ echo:
 echo Rescue Disk Will Be Downloaded from:
 echo https://rescuedisk.s.kaspersky-labs^.com/updatable/2018/krd.iso
 goto downkrd
+
 :bs
 echo !! Bootsector is missing !! .\kavrescue\boot\grub\i386-pc\eltorito.img - please use correct version of ISO!
 goto :end
+
 :downkrd
 echo:
 echo:
@@ -124,42 +134,62 @@ Echo krd.iso has been downloaded. Now updating databases.
 if exist .\kavrescue rmdir /S /Q .\kavrescue
 echo:
 goto :start
+
 :mkiso
 echo File Missing %~dp0Tools\mkisofs.exe
 echo Please re-extract "Tools" folder from downloaded zip file
 goto :end
+
 :user_stopped_the_process
 echo User stopped the process
 goto :end
+
 :not_enough_memory
 echo Not enough memory for operation
 goto :end
+
 :command_line_error
 echo Command line option error
 goto :end
+
 :fatal_error
 echo A fatal error occurred
 goto :end
+
 :ok_warnings
 echo Non fatal error(s) occurred
 goto :end
+
 :check
-IF %xmlver%%xmlpatch% GTR %isover%%isopatch% (
+if %xmlver%%xmlpatch% GTR %isover%%isopatch% (
 echo:
 echo It seems that there is a new version of Kaspersky Rescue Disk available!
-echo Version on the disk: %isover%%isopatch% ][ Version on the server: %xmlver%%xmlpatch%
+echo Version on the disk: [%isover%%isopatch%] ^| Version on the server: [%xmlver%%xmlpatch%]
 goto :input
-) ELSE ( 
+) else ( 
 goto :continue
 )
+
+:check2
+if %xmlstamp% == %isostamp% (
+title Kaspersky Rescue Disk 18 Updater
+echo:
+echo Database timestamps are equal!
+echo Timestamp on the disk: [%isostamp%] ^| Timestamp on the server: [%xmlstamp%]
+echo No update required, exiting.
+goto :cont2
+) else ( 
+goto :cont1
+)
+
 :input
 echo:
-echo Would you like to download the new version? (Y / N)
-set /p choc=
+echo Would you like to download the new version? (y or n)
+set /p choose=
 )
-if "%choc%"=="y" goto :downkrd
-if "%choc%"=="n" goto :continue
+if "%choose%"=="y" goto :downkrd
+if "%choose%"=="n" goto :continue
+
 :end
 pause
 goto :EOF
-
